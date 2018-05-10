4ec66d152f8b0fa7d933fe68a9819506e5a5047e
Loren Collingwood <loren.collingwood@ucr.edu>
2018-05-10 10:46:37 +0100

    Create clean_turk_id.R
    Added clean_turk_id function, for cleaning MTurk IDs, and reporting useful info about them.

---
+++ R/clean_turk_id.R
@@ -0,0 +1,15 @@
+clean_turk_id <- function(mtid) {
+  mtid <- str_to_lower(mtid)
+  mtid <- str_trim(mtid)
+  mtid <- car::recode(mtid, "''=NA")
+  cat("Unique number of elements:\n")
+  print(length(unique(mtid)))
+  cat("Number of elements:\n")
+  print(length(mtid))
+  cat("Number of missing IDs:")
+  print ( table(is.na(mtid)) )
+  cat("Distribution of ID Length:\nSmall numbers (< 5) are missing or possibly errors\n")
+  element_count <- as.vector(sapply(gun$MTid,str_length))
+  print( table(element_count))
+  return(mtid)
+}
