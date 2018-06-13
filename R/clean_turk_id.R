4791d06499defebf5a4fcf7ca2931af574bddfaf
Loren Collingwood <loren.collingwood@ucr.edu>
2018-06-13 11:25:49 -0700

    Update clean_turk_id.R

--- R/clean_turk_id.R
+++ R/clean_turk_id.R
@@ -12,7 +12,7 @@ clean_turk_id <- function(mtid) {
   cat("Distribution of ID Length:\nSmall numbers (< 5) are missing or possibly errors\n")
   element_count <- as.vector(sapply(mtid,str_length))
   print( table(element_count))
-  cat("\n List of duplicate IDs")
+  cat("\n List of duplicate ")
   print (mtid_out[mtid_out>1])
 
   return(mtid)
